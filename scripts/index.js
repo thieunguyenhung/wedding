726e58f1206469e4c82feb860d6e805dee8fe830
hung.thieu <hung.thieu@ubitec.com>
2021-03-17 17:57:18 +0700

    Add load image no scroll

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,40 @@
+function showImages(el) {
+  var windowHeight = jQuery(window).height();
+  $(el).each(function () {
+    console.log($(this));
+    var thisPos = $(this).offset().top;
+    var topOfWindow = $(window).scrollTop();
+    console.log('thisPos', thisPos);
+    console.log('topOfWindow', topOfWindow);
+    console.log(topOfWindow + windowHeight - 200);
+    if (topOfWindow + windowHeight - 200 > thisPos) {
+      $(this).addClass('slide-in-blurred-bottom');
+    }
+  });
+}
+
 $(document).ready(function () {
-  if (location.protocol !== 'https:') {
+  if (location.protocol !== 'https:' && location.protocol !== 'file:') {
     console.log('Redirecting to HTTPS');
     location.replace(`https:${location.href.substring(location.protocol.length)}`);
   }
   setTimeout(function () {
     $('body').show();
+    showImages('.story-img');
   }, 500);
 
-  var Scrollbar = window.Scrollbar;
-  Scrollbar.init(document.querySelector('#main-scrollbar'));
+  var scrollbar = window.Scrollbar;
+  scrollbar.init(document.querySelector('#main-scrollbar'));
+  // $(window).on('wheel', function (event) {
+  //   console.log(scrollbar.offset.y);
+  //   if (event.originalEvent.deltaY < 0) {
+  //     console.log('wheeled up');
+  //   } else {
+  //     console.log('wheeled down');
+  //   }
+  // });
+  scrollbar.getAll()[0].addListener(function (status) {
+    showImages('.story-img');
+    console.log(status.offset.y);
+  });
 });
